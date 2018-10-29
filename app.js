246eb4baf289aadebcc85b9ff1d20ddf70287e00
volosgoto <volosgoto@yahoo.com>
2018-10-29 14:53:19 +0200

    add sendmail

--- app.js
+++ app.js
@@ -13,12 +13,44 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.render('index', {title: 'Welcome'});
+	res.render('index',{title: 'Computer Not Working?'});
 });
 
 app.get('/about', function(req, res){
 	res.render('about');
 });
 
-app.listen(3030);
+app.get('/contact', function(req, res){
+	res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'volosovichmail@email.com',
+			pass: 'pass'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Anderii <volosovichmail@email.com>',
+		to: 'volosoto@yahoo.com',
+		subject: 'Website Submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
+
+app.listen(3000);
 console.log('Server is running on port 3000...');
